123a9a36a37910f53b74d9b8f9cc98ce1713bfd7
zokeya <zackokeya@gmail.com>
2022-10-15 03:54:31 -0700

    4. Hello - puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-/*
- *main function : print a line of code using puts
- *Return : 0
+/**
+ * main - print a line of code using puts
+ *
+ * Return : 0
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
