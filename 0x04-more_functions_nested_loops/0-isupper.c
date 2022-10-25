3b77bf7f1a90972c00d83367644c9eecf3b17816
zokeya <zackokeya@gmail.com>
2022-10-25 04:14:26 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check if the character is uppercase
+ * @c: Variable character
+ * Return: Always 0
+*/
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
