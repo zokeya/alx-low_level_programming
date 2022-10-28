185d409d8ae2698f3714701b9f9c2e00dd786fa5
zokeya <zackokeya@gmail.com>
2022-10-28 15:22:00 -0700

    5. Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,20 +2,20 @@
 
 /**
  * string_toupper - changes lowercase letters of a string to uppercase
- * @a: string param
+ * @s: string param
  * Return: char
 */
 
-char *string_toupper(char *a)
+char *string_toupper(char *s)
 {
 	int i;
 
 	i = 0;
-	while (*(a + i))
+	while (*(s + i))
 	{
-		if (*(a + i) >= 'a' && *(a + i) <= 'z')
-			(a + i) -= 'a' - 'A';
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) -= 'a' - 'A';
 		i++;
 	}
-	return (a);
+	return (s);
 }
