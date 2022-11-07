05c97fc3db76cfb31ecbc0e54cef5e356472281e
zokeya <zackokeya@gmail.com>
2022-11-07 10:46:52 -0800

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string param
+ * Return: int
+*/
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i++;
+		i += _strlen_recursion(s + 1);
+	}
+	return (i);
+}
