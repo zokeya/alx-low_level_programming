9c588c8d3c9095f9a737147a2c860e9c09da6e04
zokeya <zdokeya@gmail.com>
2022-12-02 22:55:42 +0300

    8. Sum list - 2

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 
 /**
- * sum_listint -  returns the sum of all the data (n) of a listint_t linked list
+ * sum_listint -  returns the sum of all the data
+ * of a listint_t linked list
  * @head: pointer to the first node
  * Return: sum all the data
- */
+*/
 
 int sum_listint(listint_t *head)
 {
