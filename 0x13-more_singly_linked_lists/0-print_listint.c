e0705ce1cbc5681ff134d4463e3847d0e7f67b40
zokeya <zdokeya@gmail.com>
2022-12-02 23:47:04 +0300

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		nodes++;
-		printf("%d\n", h-n);
+		printf("%d\n", h->n);
 		h = h->next;
+		nodes++;
 	}
 	return (nodes);
 }
