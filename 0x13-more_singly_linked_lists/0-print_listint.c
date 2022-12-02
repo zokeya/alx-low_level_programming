b5942f811764e1c482ee3c02b6dcb8daef115c41
zokeya <zdokeya@gmail.com>
2022-12-02 23:25:49 +0300

    1. List length

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,11 @@
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
 
 /**
  * print_listint - prints all the elements of a listint_t list
  * @h: head of linklist node
  * Return: the number of nodes
 */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,17 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements
- * in a linked listint_t list.
+ * listint_len - the number of elements in a linked listint_t list.
  * @h: head pointer
  * Return: the number of elements
 */
-
 size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
+
 	while (h != NULL)
 	{
-		h = h->next;
+	h = h->next;
 		count++;
 	}
 	return (count);
