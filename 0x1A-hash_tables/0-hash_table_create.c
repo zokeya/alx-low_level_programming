3779608f50f0fc190b8b08ee646b28b226282701
zokeya <zdokeya@gmail.com>
2023-03-01 19:05:49 +0300

    Initial commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,20 +7,20 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
+	hash_table_t *hash_table;
 	unsigned long int 1;
 
-	table = malloc(sizeof(hash_table_t));
-	if (table == NULL)
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
 		return (NULL);
-	table->size = size;
-	table->array = malloc(size * sizeof(hash_node_t *));
-	if (table->array == NULL)
+	hash_table->size = size;
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
 	{
-		free(table);
+		free(hash_table);
 		return (NULL);
 	}
 	for (i = 0; i < size; i++)
-		table->array[i] = NULL;
-	return (table);
+		hash_table->array[i] = NULL;
+	return (hash_table);
 }
