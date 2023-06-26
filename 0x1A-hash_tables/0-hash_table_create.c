100cfcd5b0c2900fc99efa9c06a502a8f54da1ac
zokeya <zdokeya@gmail.com>
2023-06-26 02:51:17 -0700

    Hash tables Mandatory Tasks

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,8 +2,8 @@
 
 /**
  * hash_table_create - creates a hash table
- * @size = the size of the array
- * Return a pointer to the newly created hash table
+ * @size: the size of the array
+ * Return: a pointer to the newly created hash table
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
